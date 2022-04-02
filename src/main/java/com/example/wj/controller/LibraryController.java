2067626569d9814285745b55ebd2c402b0432deb
Shana <373197139@qq.com>
2022-04-02 22:48:57 +0800

    图片上传与项目打包部署

--- src/main/java/com/example/wj/controller/LibraryController.java
+++ src/main/java/com/example/wj/controller/LibraryController.java
@@ -2,9 +2,13 @@ package com.example.wj.controller;
 
 import com.example.wj.pojo.Book;
 import com.example.wj.service.BookService;
+import com.example.wj.utils.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -38,6 +42,25 @@ public class LibraryController {
             return bookService.listByCategory(cid);
         } else {
             return list();
+        }
+    }
+
+    @CrossOrigin
+    @PostMapping("api/covers")
+    public String coversUpload(MultipartFile file) throws Exception {
+        String folder = "E:/workspace/img";
+        File imageFolder = new File(folder);
+        File f = new File(imageFolder, StringUtils.getRandomString(6) + file.getOriginalFilename()
+                .substring(file.getOriginalFilename().length() - 4));
+        if (!f.getParentFile().exists())
+            f.getParentFile().mkdirs();
+        try {
+            file.transferTo(f);
+            String imgURL = "http://localhost:8443/api/file/" + f.getName();
+            return imgURL;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
         }
     }
 
