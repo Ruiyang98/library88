d0d3b633cffc9373985d96d4da16860aa752b94d
Shana <373197139@qq.com>
2022-04-19 22:53:35 +0800

    引入Redis

--- src/main/java/com/example/wj/service/BookService.java
+++ src/main/java/com/example/wj/service/BookService.java
@@ -3,6 +3,8 @@ package com.example.wj.service;
 import com.example.wj.dao.BookDAO;
 import com.example.wj.pojo.Book;
 import com.example.wj.pojo.Category;
+import com.example.wj.redis.RedisService;
+import com.example.wj.utils.CastUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
@@ -15,18 +17,44 @@ public class BookService {
     BookDAO bookDAO;
     @Autowired
     CategoryService categoryService;
+    @Autowired
+    private RedisService redisService;
 
     public List<Book> list() {
-        Sort sort = Sort.by(Sort.Direction.DESC, "id");
-        return bookDAO.findAll(sort);
+        List<Book> books;
+        String key = "booklist";
+        Object bookCache = redisService.get(key);
+
+        if (bookCache == null) {
+            Sort sort = Sort.by(Sort.Direction.DESC, "id");
+            books = bookDAO.findAll(sort);
+            redisService.set(key, books);
+        } else {
+            books = CastUtils.objectConvertToList(bookCache, Book.class);
+        }
+        return books;
     }
 
     public void addOrUpdate(Book book) {
+        redisService.delete("booklist");
         bookDAO.save(book);
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        redisService.delete("booklist");
     }
 
     public void deleteById(int id) {
+        redisService.delete("booklist");
         bookDAO.deleteById(id);
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        redisService.delete("booklist");
     }
 
     public List<Book> listByCategory(int cid) {
--- src/main/java/com/example/wj/service/JotterArticleService.java
+++ src/main/java/com/example/wj/service/JotterArticleService.java
@@ -2,6 +2,8 @@ package com.example.wj.service;
 
 import com.example.wj.dao.JotterArticleDAO;
 import com.example.wj.pojo.JotterArticle;
+import com.example.wj.redis.RedisService;
+import com.example.wj.utils.MyPage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -10,25 +12,60 @@ import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+
 @Service
 public class JotterArticleService {
     @Autowired
     JotterArticleDAO jotterArticleDAO;
 
-    public Page list(int page, int size) {
-        Sort sort = Sort.by(Sort.Direction.DESC, "id");
-        return  jotterArticleDAO.findAll(PageRequest.of(page, size, sort));
+    @Autowired
+    RedisService redisService;
+
+    // MyPage 是自定义的 Spring Data JPA Page 对象的替代
+    public MyPage list(int page, int size) {
+        MyPage<JotterArticle> articles;
+        // 用户访问列表页面时按页缓存文章
+        String key = "articlepage:" + page;
+        Object articlePageCache = redisService.get(key);
+
+        if (articlePageCache == null) {
+            Sort sort = Sort.by(Sort.Direction.DESC, "id");
+            Page<JotterArticle> articlesInDb = jotterArticleDAO.findAll(PageRequest.of(page, size, sort));
+            articles = new MyPage<>(articlesInDb);
+            redisService.set(key, articles);
+        } else {
+            articles = (MyPage<JotterArticle>) articlePageCache;
+        }
+        return articles;
     }
 
     public void addOrUpdate(JotterArticle article) {
         jotterArticleDAO.save(article);
+
+        redisService.delete("article" + article.getId());
+        Set<String> keys = redisService.getKeysByPattern("articlepage*");
+        redisService.delete(keys);
     }
 
     public JotterArticle findById(int id) {
-        return  jotterArticleDAO.findById(id);
+        JotterArticle article;
+        String key = "article:" + id;
+        Object articleCache = redisService.get(key);
+
+        if (articleCache == null) {
+            article = jotterArticleDAO.findById(id);
+            redisService.set(key, article);
+        } else {
+            article = (JotterArticle) articleCache;
+        }
+        return article;
     }
 
     public void delete(int id) {
         jotterArticleDAO.deleteById(id);
+
+        redisService.delete("article:" + id);
+        Set<String> keys = redisService.getKeysByPattern("articlepage*");
+        redisService.delete(keys);
     }
 }
