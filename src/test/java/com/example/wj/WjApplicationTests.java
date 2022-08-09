1a2a0fc2d01bea6c1dfd8e631140a437bfa9914d
Shana <373197139@qq.com>
2022-08-09 10:05:27 +0800

    引入单元测试

--- src/test/java/com/example/wj/WjApplicationTests.java
+++ src/test/java/com/example/wj/WjApplicationTests.java
@@ -1,13 +1,29 @@
 package com.example.wj;
 
+
+import com.example.wj.service.BookService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.BeansException;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
-class WjApplicationTests {
+@ContextConfiguration(classes = WjApplication.class)
+class WjApplicationTests implements ApplicationContextAware {
 
-    @Test
-    void contextLoads() {
+    private ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
     }
 
+    @Test
+    public void testApplicationContext() {
+        System.out.println(applicationContext);
+        BookService bookService = applicationContext.getBean(BookService.class);
+        System.out.println(bookService.list());
+    }
 }
